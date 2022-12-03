f66ea903b5065e58e90ac68800d372b578e32f8b
Richard L <rail.rich@gmail.com>
2022-12-02 21:04:37 -0500

    Added a new but ugly solution

--- aoc2022/src/day2.fsx
+++ aoc2022/src/day2.fsx
@@ -6,34 +6,34 @@ let inputs = System.IO.File.ReadLines("inputs/day2-input.txt")
 let strategy1 (game:string) : int =
     match (game) with
     //Win
-    | ("A Y") -> 6 + 2
-    | ("B Z") -> 6 + 3
-    | ("C X") -> 6 + 1
+    | "A Y" -> 6 + 2
+    | "B Z" -> 6 + 3
+    | "C X" -> 6 + 1
     // Tie
-    | ("A X") -> 3 + 1
-    | ("B Y") -> 3 + 2
-    | ("C Z") -> 3 + 3
+    | "A X" -> 3 + 1
+    | "B Y" -> 3 + 2
+    | "C Z" -> 3 + 3
     // Loose
-    | ("A Z") -> 0 + 3
-    | ("B X") -> 0 + 1
-    | ("C Y") -> 0 + 2
+    | "A Z" -> 0 + 3
+    | "B X" -> 0 + 1
+    | "C Y" -> 0 + 2
     | _ -> 0
 
 
 let strategy2 (game:string) : int =
     match (game) with
     //Win
-    | ("A Z") -> 6 + 2
-    | ("B Z") -> 6 + 3
-    | ("C Z") -> 6 + 1
+    | "A Z" -> 6 + 2
+    | "B Z" -> 6 + 3
+    | "C Z" -> 6 + 1
     // Tie
-    | ("A Y") -> 3 + 1
-    | ("B Y") -> 3 + 2
-    | ("C Y") -> 3 + 3
+    | "A Y" -> 3 + 1
+    | "B Y" -> 3 + 2
+    | "C Y" -> 3 + 3
     // Loose
-    | ("A X") -> 0 + 3
-    | ("B X") -> 0 + 1
-    | ("C X") -> 0 + 2
+    | "A X" -> 0 + 3
+    | "B X" -> 0 + 1
+    | "C X" -> 0 + 2
     | _ -> 0
 
 
--- aoc2022/src/day2Algo2.fsx
+++ aoc2022/src/day2Algo2.fsx
@@ -1,45 +1,38 @@
 printfn "Hello from F#"
 
-// let inputs = System.IO.File.ReadLines("inputs/day2-exemple1.txt")
-let inputs = System.IO.File.ReadLines("inputs/day2-input.txt")
+let inputs = System.IO.File.ReadLines("examples/day2/exemple6.txt")
+// let inputs = System.IO.File.ReadLines("inputs/day2-input.txt")
 
-let strategy1 (col1: char, col2: char) : int =
-    match (col1, col2) with
-    //Win
-    | ('A', 'Y') -> 6 + 2
-    | ('B', 'Z') -> 6 + 3
-    | ('C', 'X') -> 6 + 1
-    // Tie
-    | ('A', 'X') -> 3 + 1
-    | ('B', 'Y') -> 3 + 2
-    | ('C', 'Z') -> 3 + 3
-    // Loose
-    | ('A', 'Z') -> 0 + 3
-    | ('B', 'X') -> 0 + 1
-    | ('C', 'Y') -> 0 + 2
+let handShapeToPoint(handShape:char) = 
+    match handShape with
+    | 'A' | 'X' -> 1
+    | 'B'| 'Y' -> 2
+    | 'C'| 'Z' -> 3
     | _ -> 0
 
-
-let strategy2 (col1: char, col2: char) : int =
-    match (col1, col2) with
-    //Win
-    | ('A', 'Z') -> 6 + 2
-    | ('B', 'Z') -> 6 + 3
-    | ('C', 'Z') -> 6 + 1
-    // Tie
-    | ('A', 'Y') -> 3 + 1
-    | ('B', 'Y') -> 3 + 2
-    | ('C', 'Y') -> 3 + 3
-    // Loose
-    | ('A', 'X') -> 0 + 3
-    | ('B', 'X') -> 0 + 1
-    | ('C', 'X') -> 0 + 2
+let handShapeThatKills(handShape:int) = 
+    match handShape with
+    | 1 -> 2
+    | 2 -> 3
+    | 3 -> 1
     | _ -> 0
 
-let fromStringToTuple (str: string) : char * char =
-    let strArray = str.Split(" ")
-    (strArray.[0].ToCharArray().[0], strArray.[1].ToCharArray().[0])
+let strategy1 (evil:int, good:int) : int =
+    if evil = good then
+        3 + good
+    elif handShapeThatKills(evil) = good then
+        6 + good
+    else
+        0 + good
 
-let inputToMoves = inputs |> Seq.map fromStringToTuple
-inputToMoves |> Seq.map (strategy1) |> Seq.sum |> printfn "Strategy 1: %d"
-inputToMoves |> Seq.map (strategy2) |> Seq.sum |> printfn "Strategy 2: %d"
+let strategy2 (evil:int, good:int) : int =
+    if good = 1 then 
+        0 + handShapeThatKills(handShapeThatKills(evil))
+    elif good = 2 then //Draw
+        3 + evil
+    else
+        6 + handShapeThatKills(evil)
+        
+let evilVSGood = inputs |> Seq.map (fun game -> handShapeToPoint(game.[0]) , handShapeToPoint(game.[2]))
+evilVSGood |> Seq.map (strategy1) |> Seq.sum |> printfn "Strategy 1: %d"
+evilVSGood |> Seq.map (strategy2) |> Seq.sum |> printfn "Strategy 2: %d"
